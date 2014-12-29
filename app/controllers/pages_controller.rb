125db6616c1151db7b9228fe2fbd26d6ef1282a4
Carlos Lazo <carlos.jose.lazo@gmail.com>
2014-12-28 22:30:02 -0500

    Adds three (3) design options for splash page.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,5 @@
 class PagesController < ApplicationController
   def index; end
+
+  3.times { |i| define_method("design#{i+1}"){} }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#index'
+
+  3.times { |i| get "/design#{i+1}", to: "pages#design#{i+1}", as: "design#{i+1}" }
 end
